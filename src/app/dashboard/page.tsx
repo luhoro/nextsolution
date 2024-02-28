4419bf3cdc3985253e17f0cc97dd134b556fca97
luhoro <luisashorowitz@gmail.com>
2024-02-27 22:01:30 -0300

    Build: Install database and orm

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import Container from "@/components/header/container"
+import React from "react"
+
+const Dashboard = () => {
+  return (
+    <Container>
+      <h1>aaaa</h1>
+    </Container>
+  )
+}
+
+export default Dashboard
