0c2bb71b346421105419af9b65088bbb4915b271
luhoro <luisashorowitz@gmail.com>
2024-03-03 16:24:08 -0300

    Feat: Login and logout session

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,4 +1,4 @@
-import Container from "@/components/header/container"
+import Container from "@/components/container"
 import React from "react"
 
 const Dashboard = () => {
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,7 +1,12 @@
+"use client"
+
 import Link from "next/link"
-import { FiUser, FiLogOut } from "react-icons/fi"
+import { FiUser, FiLogOut, FiLoader, FiLock } from "react-icons/fi"
+import { signIn, signOut, useSession } from "next-auth/react"
 
 const Header = () => {
+  const { status, data } = useSession()
+
   return (
     <header className="flex justify-center bg-purple-50  border-b">
       <div className="w-full flex items-center justify-between max-w-7xl px-5 py-6">
@@ -12,15 +17,45 @@ const Header = () => {
           </h1>
         </Link>
 
-        <div className="flex gap-6">
-          <Link href="/dashboard">
-            <FiUser size={28} color="#763ad8" className="hover:opacity-70 duration-300"/>
-          </Link>
+        {status === "loading" && (
+          <button>
+            <FiLoader
+              size={28}
+              color="#763ad8"
+              className="animate-spin"
+            />
+          </button>
+        )}
 
-          <Link href="/">
-            <FiLogOut size={28} color="#763ad8" className="hover:opacity-70 duration-300"/>
-          </Link>
-        </div>
+        {status === "unauthenticated" && (
+          <button onClick={async () => await signIn()}>
+            <FiLock
+              size={28}
+              color="#763ad8"
+              className="hover:opacity-70 duration-300"
+            />
+          </button>
+        )}
+
+        {status === "authenticated" && (
+          <div className="flex gap-6">
+            <Link href="/dashboard">
+              <FiUser
+                size={28}
+                color="#763ad8"
+                className="hover:opacity-70 duration-300"
+              />
+            </Link>
+
+            <Link href="/" onClick={async () => await signOut()}>
+              <FiLogOut
+                size={28}
+                color="#9A6FDF"
+                className="hover:opacity-70 duration-300"
+              />
+            </Link>
+          </div>
+        )}
       </div>
     </header>
   )
