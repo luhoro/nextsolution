410830b0a08239ba84e393c8f73f713ed721a8c7
luhoro <luisashorowitz@gmail.com>
2024-03-15 22:14:19 -0300

    Feat: Delete clients database

--- src/app/api/customer/route.ts
+++ src/app/api/customer/route.ts
@@ -5,7 +5,6 @@ import prismaClient from "@/lib/prisma"
 
 export const POST = async (request: Request) => {
   const session = await getServerSession(authOptions)
-
   if (!session || !session.user) {
     return NextResponse.json({ error: "Not authorized" }, { status: 401 })
   }
@@ -13,19 +12,48 @@ export const POST = async (request: Request) => {
   const { name, email, phone, address, userId } = await request.json()
 
   try {
-    await prismaClient.customer.create({ 
+    await prismaClient.customer.create({
       data: {
         name,
         phone,
         email,
         address: address ? address : "",
-        userId
-      }
-     })
+        userId,
+      },
+    })
 
-     return NextResponse.json({ message: "Cliente cadastrado com sucesso!" })
+    return NextResponse.json({ message: "Cliente cadastrado com sucesso!" })
   } catch (err) {
-    return NextResponse.json({ error: "Failed create new customer" }, { status: 400 })
+    return NextResponse.json(
+      { error: "Failed create new customer" },
+      { status: 400 }
+    )
+  }
+}
+
+export const DELETE = async (request: Request) => {
+  const session = await getServerSession(authOptions)
+  if (!session || !session.user) {
+    return NextResponse.json({ error: "Not authorized" }, { status: 401 })
   }
 
+  const { searchParams } = new URL(request.url)
+  const userId = searchParams.get("id")
+
+  if (!userId) {
+    return NextResponse.json({ error: "Fail delete customer" }, { status: 401 })
+  }
+
+  try {
+    await prismaClient.customer.delete({
+      where: {
+        id: userId as string,
+      },
+    })
+    return NextResponse.json({ message: "Sucess delete customer" })
+
+  } catch (error) {
+    console.log(error)
+    return NextResponse.json({ error: "Fail delete customer" }, { status: 401 })
+  }
 }
--- src/app/dashboard/customer/components/card.tsx
+++ src/app/dashboard/customer/components/card.tsx
@@ -1,7 +1,27 @@
+"use client"
+
 import { FiTrash2 } from "react-icons/fi"
 import CustomerProps from "@/utils/customer.type"
+import api from "@/lib/api"
+import { useRouter } from "next/navigation"
 
 const Card = ({ customer }: { customer: CustomerProps }) => {
+  const router = useRouter()
+
+
+  const handleDeleteCustomer = async () => {
+    try {
+      const response = await api.delete("/api/customer", {
+        params: {
+          id: customer.id,
+        },
+      })
+      router.refresh()
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
     <article className="w-full justify-between items-start flex p-3 border rounded-lg hover:bg-gray-50 duration-200">
       <div>
@@ -18,7 +38,10 @@ const Card = ({ customer }: { customer: CustomerProps }) => {
         </p>
       </div>
 
-      <button className="w-fit  hover:-translate-y-1 duration-200 rounded-md font-bold">
+      <button
+        onClick={handleDeleteCustomer}
+        className="w-fit  hover:-translate-y-1 duration-200 rounded-md font-bold"
+      >
         <FiTrash2 fill="#F0D6D6" size={24} />
       </button>
     </article>
